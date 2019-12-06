55ab0300216b39daf377c20ab98d6ed622664940
Charles Willian <wcharlesw15@gmail.com>
2019-12-06 07:03:16 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main/index';
+import Repository from './pages/Repository/index';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* o switch garante que apenas uma rota se chamada por vez */}
+        <Route path="/" exact component={Main} />
+        {/* se o exact não for infomado, o react irá mostrar a primeira rota que comece com "/", ou seja, nenhuma rota seria mostrada */}
+        <Route path="/repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
